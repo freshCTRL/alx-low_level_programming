02381ea3437e19b70f4ad5c4ccfe4faa2a54e360
freshCTRL <oyeboabariu25@gmail.com>
2022-09-10 14:10:06 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description - function to print a line of quote
+ *
+ * Return - A;ways 0 (sucess)
+ */ 
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
