0115347361b6096960b07b6c8778dde3e8c16d6c
freshCTRL <oyeboabariu25@gmail.com>
2022-09-10 14:26:10 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Description - function to print a line
- *
+ * main - outputs \"Programming is like building a multilingual puzzle
  * return - Always 0 (sucess)
  */
 
