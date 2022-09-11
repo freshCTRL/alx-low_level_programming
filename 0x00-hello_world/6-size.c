bc58e73fef843b94613a253e76efe3d14ec07dbb
freshCTRL <oyeboabariu25@gmail.com>
2022-09-11 08:38:27 -0700

    message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float f;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(g));
-printf("size of a long long int: %lu bytes\n", (unsigned long)sizeof(h));
-printf("size of a float: %lu bytes\n", (unsigned long)sizeof(f));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(h));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
