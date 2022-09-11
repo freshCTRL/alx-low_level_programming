823a392173f61a1e61906389dab7f4126e00eb43
freshCTRL <oyeboabariu25@gmail.com>
2022-09-10 23:32:18 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * main - print the string in the puts function
  *
  * Description - using the main function 
- * this program prints "\"Programming is like building a multilingual puzzle" 
- * return - Always 0 (sucess)
+ * this program prints "Programming is like building a multilingual puzzle 
+ * Return - 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
