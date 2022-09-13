013c05b3076197ca12c183edb7669e7260e89144
freshCTRL <oyeboabariu25@gmail.com>
2022-09-12 20:22:43 -0700

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 putchar(ch);
 }
-for(ch = 'A' ; ch <= 'Z' ; ch++)
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
 putchar(ch);
 }
