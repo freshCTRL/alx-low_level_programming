4a45760b9fe36c043b5471858e2f33a3e57e8cfc
freshCTRL <oyeboabariu25@gmail.com>
2022-09-12 20:14:38 -0700

    message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+char c;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+}
+putchar('\n')	
+return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+char ch;
+for ch(ch = 'a' ; ch <= 'z' ; ch++)
+{	
+if (ch != 'a' && ch != 'q');
+{
+putchar(ch);
+}
+}
+putchar(\n);
+return (0);
+}
+
