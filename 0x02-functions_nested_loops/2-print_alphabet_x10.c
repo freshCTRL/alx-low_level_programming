4cc17bfe98bcf8146039f3cd5689cbf277c78dfb
freshCTRL <oyeboabariu25@gmail.com>
2022-09-13 05:53:32 -0700

    message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+int counter = 0;
+int haltcounter = 0;
+
+while (haltcounter == 0)
+{
+if (counter < 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+counter++;
+}
+else
+{
+haltcounter = 1;
+}
+}
+return (0);
+}
