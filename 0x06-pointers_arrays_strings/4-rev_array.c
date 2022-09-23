dcfd66a4cdaba6addf3e7ffac61106818ac027ab
freshCTRL <oyeboabariu25@gmail.com>
2022-09-22 23:49:30 -0700

    message

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+int *p, i, aux, k;
+p = a;
+for (i = 1; i < n; i++)
+{
+p++;
+}
+for (k = 0; k < i / 2; k++)
+{
+aux = a[k];
+a[k] = *p;
+*p = aux;
+p--;
+}
+}
