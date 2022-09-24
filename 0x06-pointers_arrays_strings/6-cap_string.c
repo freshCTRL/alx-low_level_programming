29aa5d02a9ba36beefb182aed7e1b5302729911c
freshCTRL <oyeboabariu25@gmail.com>
2022-09-24 02:07:00 -0700

    message

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  *
  * Return: A pointer to the changed string
  */
-*cap_string(char *str)
+char *cap_string(char *str)
 {
 int index = 0;
 while (str[index])
@@ -14,10 +14,21 @@ while (str[index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 {
-if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' index == 0)
-{
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}'
+index == 0)
 str[index] -= 32;
-}
 }
 index++;
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * leet - function that encodes a string into 1337.
- * @str: string to encode
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
  *
- * Return: A pointer to the encoded string
+ * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
 {
@@ -12,12 +12,10 @@ int indx1 = 0, indx2;
 char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 while (str[indx1])
 {
-for (indx2 = 0, indx2 <= 7, indx2++)
+for (indx2 = 0; indx2 <= 7; indx2++)
 {
-if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
-{
+if (str[indx1] == leet[indx2]str[indx1] - 32 == leet[indx2])
 str[indx1] = indx2 + '0';
-}
 }
 indx1++;
 }
