1e38edc7ab9d70cef498a9606e1cad3ba88d7045
freshCTRL <oyeboabariu25@gmail.com>
2022-09-25 07:38:03 -0700

    message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - causes an infinite loop.
+ *
  * Return: no return value
  */
 int main(void)
 {
 int i;
-
-printf("Infinite loop incoming :(\n");
 i = 0;
 
+printf("Infinite loop incoming :(\n");
 /*while (i < 10)*/
 {
 /*putchar(i);*/
