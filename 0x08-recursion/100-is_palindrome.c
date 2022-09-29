2e04e410576ac587642491580e47cec6816d302f
freshCTRL <oyeboabariu25@gmail.com>
2022-09-29 16:40:57 -0700

    message

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -46,7 +46,7 @@ return (0);
 /**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
- * 
+ *
  * Return: If the string is a palindrome - 1.
  * If the string is not a palindrome - 0.
  */
