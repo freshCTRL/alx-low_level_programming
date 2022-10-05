3e5412311fa8567a44a36feb41a7b4a292341651
freshCTRL <oyeboabariu25@gmail.com>
2022-10-05 09:19:19 -0700

    ending

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File:
- * Auth:
- *
- * Desc:
- */
 
 int _putchar(char c);
 int _islower(int c);
@@ -14,8 +8,8 @@ int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
-int _strlen(char *s;
-void _puts(char *s;
+int _strlen(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -27,6 +21,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
