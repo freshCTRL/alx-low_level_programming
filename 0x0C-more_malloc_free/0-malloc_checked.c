7547ae00fd8a9331add681ff9461946ee6220056
freshCTRL <oyeboabariu25@gmail.com>
2022-10-07 22:04:39 -0700

    message

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,10 @@
 #include <stdlib.h>
 
 /**
- *
  * malloc_checked - Allocates memory using malloc.
- *
  * @b: The number of bytes to be allocated.
  *
- *
- * Return: A pointer to the allocated memory.
+ *Return: A pointer to the allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
