9923a577b0061e8e697f8090335e8605578eef8a
freshCTRL <oyeboabariu25@gmail.com>
2022-10-11 23:14:34 -0700

    messag

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,10 +3,11 @@
 
 /*
  * File: 3-function_like_macro.h
- * AUTH:Opeyemi OYEBOADE
- * Desc: Header file that defines a function-like macro ABS(x)
- * that computes the absolute value of a number x.
+ * Auth: Ismail Mohamed Ismail
+ * Desc: Header file that defines a function-like macro ABS(x) that
+ * computes the absolute value of a number x.
  */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
