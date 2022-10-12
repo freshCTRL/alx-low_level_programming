89d5828903a322c3fde962b9bba360e6f6cbaf66
freshCTRL <oyeboabariu25@gmail.com>
2022-10-11 22:49:02 -0700

    message

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,8 +2,9 @@
 #define 1_PI_H
 
 /*
- * AUTH: Opeyemi OYEBOADE
- *
+ * AUTH:Opeyemi OYEBOADE
+ * Desc: Header file that defines a macro named PI as an
+ * abbreviation for the token 3.14159265359.
  */
 #define PI 3.14159265359
 
