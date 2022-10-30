47caa5312fa5f21c9cdae3b3b1aed2a872ce18f1
freshCTRL <oyeboabariu25@gmail.com>
2022-10-29 22:23:03 -0700

    message

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index
+ * get_bit - gets the value of a bit at a given index
  * @n: the number
  * @index: the index
  *
  * Return: returns integer
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned int i;
 
 if (index > 63)
 return (-1);
-i = 1 << index;
-*n = (*n | i);
-return (1);
+
+return ((n >> index) & 1);
 }
