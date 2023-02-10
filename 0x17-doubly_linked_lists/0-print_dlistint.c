b2b0bb38451809a5ec51f1a2670492bbf8d41ca2
freshCTRL <oyeboabariu25@gmail.com>
2023-02-10 03:47:33 -0800

    message

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that prints all the elements of a dlistint_t list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count = 0;
+
+if (h == NULL)
+	printf("Linked List is empty");
+while (h != NULL)
+{
+	count++;
+	printf("%d\n", h->n);
+	h = h->next;
+}
+return (count);
+}
