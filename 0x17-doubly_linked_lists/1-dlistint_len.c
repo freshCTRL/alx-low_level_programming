e4cf1ba4f80e7176e80a2e71b2e17813a9ea0bf2
freshCTRL <oyeboabariu25@gmail.com>
2023-02-10 03:51:10 -0800

    message

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - a function that returns the number of elements in a linked dlistint_t list
+ * dlistint_len - a function that returns the number of
+ * elements in a linked dlistint_t list
  * @h: pointer to the list
  *
  * Return: number of elements
