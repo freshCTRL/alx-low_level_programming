5bd131e6180395b8fe0354faa93c493162e7499c
freshCTRL <oyeboabariu25@gmail.com>
2023-03-04 21:44:42 -0800

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-int i;
-i = 0;
+/* int i; */
+/* i = 0; */
 
 printf("Infinite loop incoming :(\n");
 /* while (i < 10) */
-{
-putchar(i);
-}
+/* { */
+/* putchar(i); */
+/* } */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
