e2d14fbdb79041d2d74b0d347128790c964d3c45
freshCTRL <oyeboabariu25@gmail.com>
2023-03-18 01:32:05 -0700

    dbl

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,7 +2,9 @@
 /**
  * free_dlistint - a FUNCTION THAT FREEs DOUBLE LINKED LIST
  * @head: argument to free_dlistint_t
- **/
+ *
+ * Description: free function
+ */
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *temp;
@@ -11,8 +13,8 @@ while (head->next != NULL)
 {
 temp = head;
 head = temp->next;
-head->prev = NULL;
 temp->next = NULL;
+head->prev = NULL;
 free(temp);
 temp = NULL;
 }
