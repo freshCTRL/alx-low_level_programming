08a0e655ecd01b5b4fa30b27ce53f8d00b4d2cc7
freshCTRL <oyeboabariu25@gmail.com>
2023-03-18 01:00:12 -0700

    dbl

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,18 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
  * free_dlistint - a FUNCTION THAT FREEs DOUBLE LINKED LIST
  * @head: argument to free_dlistint_t
  *
- * Description: set the head of a linked list to null
+ * Description: it takes the address of the head node, fees it and set it to null
  */
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *temp;
 
+if (head = NULL)
+{
+return;
+}
 while (head->next != NULL)
 {
 temp = head;
