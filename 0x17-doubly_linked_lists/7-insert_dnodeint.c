e7739ba6eeffea4fc66da3368debd2be6067812c
freshCTRL <oyeboabariu25@gmail.com>
2023-04-09 09:19:41 -0700

    dbl

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,22 +5,21 @@
  * @idx: argument to insert_dnodeint_at_index function
  * @n: argument to insert_dnodeint_at_index function
  * Return: NULL (Success)
-*/
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *temp = malloc(sizeof(dlistint_t));
-dlistint_t *tp, *ptr = *h;
+dlistint_t *tp = *h, *ptr = *h;
 unsigned int k, j;
 for (k = 0; ptr != NULL; k++)
 ptr = ptr->next;
 if (idx > k)
-return (NULL);
+return NULL;
 if (temp == NULL)
 return (NULL);
 temp->n = n;
 if (*h != NULL)
 {
-tp = *h;
 if (idx == 0)
 {    /* insertion at the beginnning */
 temp->next = *h;
