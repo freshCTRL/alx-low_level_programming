d8cd56a6648456d7c1510877617fb34e7fcf5803
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-10 11:43:42 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *my_table = malloc(sizeof(hash_table_t));
+hash_table_t *my_table = malloc(sizeof(hash_table_t * size));
 my_table->array = malloc(sizeof(hash_node_t));
 if (size == 0)
 return (NULL);
