376a95ee96662e61b2b81ecc55b5c353947fc332
freshCTRL <oyeboabariu25@gmail.com>
2023-04-09 23:30:39 -0700

    hashtables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Entry point
+ * @size: argument to hash_table_create function
+ * Return: NULL if malloc fails else address of newtable.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *my_table = malloc(sizeof(hash_table_t));
+if (my_table == NULL)
+return NULL;
+my_table->size = size;
+return my_table;
+}
