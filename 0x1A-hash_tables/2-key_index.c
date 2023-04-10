2ed5a1b63ff2a6ba2c8a5c165e48ab51624bedf0
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 00:57:12 -0700

    hash_tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - Entry point.
+ * @key: argument to key_index function
+ * @size: argument to key_index function
+ * Return: the index value.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int s = hash_djb2(key);
+unsigned long int idx = s % size;
+return (idx);
+}
