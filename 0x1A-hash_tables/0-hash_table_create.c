24ba6847844e3669d59a642d8d0f11fdc7a8da7f
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 03:03:12 -0700

    hashtables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *my_table = calloc(sizeof(hash_table_t), size);
-my_table->array = malloc(sizeof(hash_node_t));
+my_table->array = calloc(sizeof(hash_node_t), 24);
 if (size == 0)
 return (NULL);
 if (my_table == NULL)
