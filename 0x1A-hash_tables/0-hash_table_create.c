19f7098bdabb80d633763c59bb7f6084d524700e
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 03:06:07 -0700

    hashtables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *my_table = calloc(sizeof(hash_table_t), size);
-my_table->array = calloc(sizeof(hash_node_t), 24);
+my_table->array = calloc(sizeof(hash_node_t), 8);
 if (size == 0)
 return (NULL);
 if (my_table == NULL)
