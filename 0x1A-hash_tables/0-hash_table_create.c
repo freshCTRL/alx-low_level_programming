4b2aaf800395d55971e40bc20f077f41d1bc0e00
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 06:23:32 -0700

    hashtables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
 hash_table_t *my_table = malloc(sizeof(hash_table_t));
-my_table->array = malloc(sizeof(hash_node_t) * size);
 
 if (my_table == NULL)
 return (NULL);
@@ -18,6 +17,7 @@ free(my_table);
 return (NULL);
 }
 my_table->size = size;
+my_table->array = malloc(sizeof(hash_node_t) * my_table->size);
 for (i = 0; i < size; i++)
 my_table->array[i] = NULL;
 
