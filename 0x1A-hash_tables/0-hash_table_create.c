4cecc80d0485436c3be14e6652e641190f10050e
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-10 11:39:25 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,11 +2,10 @@
 /**
  * hash_table_create - Entry point
  * @size: argument to hash_table_create function
- * Return: NULL if malloc fails else address of newtable.
- */
+ * Return: NULL if malloc fails else address of newtable. */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *my_table = malloc(8 * size);
+hash_table_t *my_table = malloc(sizeof(hash_table_t));
 my_table->array = malloc(sizeof(hash_node_t));
 if (size == 0)
 return (NULL);
