b66c56d15fea3326a3a018eb412143e0d3327a18
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 06:08:37 -0700

    hashtables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,24 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 /**
  * hash_table_create - Entry point
  * @size: argument to hash_table_create function
  * Return: NULL if malloc fails else address of newtable.
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *my_table = malloc(sizeof(hash_table_t));
+unsigned int i = 0;
+hash_table_t *my_table = malloc(sizeof(*my_table));
 if (my_table == NULL)
 return (NULL);
 my_table->size = size;
-my_table->array = calloc(my_table->size, sizeof(hash_node_t));
-if (my_table->array == NULL)
-{
-free(my_table);
-return (NULL);
+my_table->array = malloc(sizeof(hash_node_t) * my_table->size);
+if (my_table->array == NULL) {
+    free(my_table);
+    return (NULL);
+}
+while (i < my_table->size) {
+my_table->array[i] = NULL;
+i++;
 }
 return (my_table);
 }
