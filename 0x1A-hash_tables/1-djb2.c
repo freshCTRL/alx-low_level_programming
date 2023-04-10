358fa6fe02b11f68b386373e48dd689d8a50f922
freshCTRL <oyeboabariu25@gmail.com>
2023-04-10 00:26:28 -0700

    hashtables

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - Entry point
+ * @str: argument to hash_djb2 function
+ * Return: hash value
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+hash = ((hash << 5) + hash)+c; /* hash * 33 + c */
+return (hash);
+}
