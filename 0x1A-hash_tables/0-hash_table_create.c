4c135cb23c9c708d37b0f1fc733ec0f773c30f9e
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-04-11 04:00:30 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,34 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_node_t *array;
 hash_table_t *table;
 
-table = malloc(((size / 2) + 1) * 15);
+array->key = "";
+array->value = "";
+array->next = NULL;
+
+array = malloc(sizeof(hash_node_t));
+table = malloc(1 * sizeof(hash_table_t);
 
 if (table == NULL)
 return (NULL);
+
+table->size = size;
+table->array = malloc(size * sizeof(array));
+
+if (table->array == NULL)
+{
+free(table);
+table = NULL;
+return (NULL);
+}
+
+i = 0;
+for (; i < size; ++i)
+{
+table->array[i] = array;
+}
 
 return (table);
 }
