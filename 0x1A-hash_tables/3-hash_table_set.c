9acc8b5b8837beca72b8842ff82fe95c42783981
freshCTRL <oyeboabariu25@gmail.com>
2023-04-16 10:36:59 -0700

    hashtables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,8 +9,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int idx;
-int stp = 0;
-hash_node_t *new_entry = NULL;
 if (ht == NULL)
 return (0);
 if (ht->array == NULL)
@@ -33,26 +31,13 @@ else
 {
 if (strcmp(ht->array[idx]->key, key) != 0)
 {
-if (ht->array[idx]->next != NULL)
-while ((ht->array[idx]) && (stp != 1))
-{
-ht->array[idx] = ht->array[idx]->next;
-if (strcmp(ht->array[idx]->key, key) == 0)
-{
-ht->array[idx]->value = strdup(value);
-stp = 1;
-}
-}
-if (stp != 1)
-{
-new_entry = malloc(sizeof(ht->size));
+hash_node_t *new_entry = malloc(sizeof(ht->size));
 if (new_entry == NULL)
 return (0);
 new_entry->key = strdup(key);
 new_entry->value = strdup(value);
 new_entry->next = ht->array[idx];
 ht->array[idx] = new_entry;
-}
 }
 else
 {
