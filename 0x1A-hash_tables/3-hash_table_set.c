b607a5c82cb6c56daaf96fced375bc5df9a0888c
freshCTRL <oyeboabariu25@gmail.com>
2023-04-16 00:37:52 -0700

    hashtables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,6 +5,7 @@
  * @ht: argument to hash_table_set
  * @key:argument to hash_table_set
  * @value: argument to hash_table_set
+ *
  * Return: 0 if failed else 1
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -23,7 +24,7 @@ if (idx < ht->size)
 if (ht->array[idx] == NULL)
 {
 ht->array[idx] = malloc(sizeof(ht->size));
-if (ht->array[idx] == NULL) /*when the array index is null*/
+if (ht->array[idx] == NULL)
 return (0);
 ht->array[idx]->key = strdup(key);
 ht->array[idx]->value = strdup(value);
@@ -31,79 +32,16 @@ ht->array[idx]->next = NULL;
 }
 else
 {
+hash_node_t *new_entry = malloc(sizeof(ht->size));
+if (new_entry == NULL)
+return (0);
+new_entry->key = strdup(key);
+new_entry->value = strdup(value);
+new_entry->next = ht->array[idx];
+ht->array[idx] = new_entry;
+}
+}
 if (strcmp(ht->array[idx]->key, key) == 0)
-{
-/**
-*when the array idx is not null and
-*the key are thesame and hash to the same value(idx)
-*/
-hash_node_t *new_entry = malloc(sizeof(ht->size));
-if (new_entry == NULL)
-return (0);
-new_entry->key = strdup(key);
-new_entry->value = strdup(value);
-new_entry->next = ht->array[idx];
-ht->array[idx] = new_entry;
-}
-else
-{
-/**
-*when the array index is not null and keys
-*are different but hash to the same value(idx)
-*/
-stp = 0;
-for (i = 0; ((i < ht->size) && (stp != 1)); i++)
-{
-if (ht->array[i] != NULL)
-{
-if (strcmp(ht->array[i]->key, key) == 0)
-{
-ht->array[i] = malloc(sizeof(ht->size));
-if (ht->array[i] == NULL)
-return (0);
-ht->array[i]->key = strdup(key);
-ht->array[i]->value = strdup(value);
-ht->array[i]->next = NULL;
-stp = 1;
-}
-}
-}
-if (stp != 1)
-{
-for (i = 0; ((i < ht->size) && (stp != 1)); i++)
-{
-if (ht->array[i] == NULL)
-{
-ht->array[i] = malloc(sizeof(ht->size));
-if (ht->array[i] == NULL)
-return (0);
-ht->array[i]->key = strdup(key);
-ht->array[i]->value = strdup(value);
-ht->array[i]->next = NULL;
-stp = 1;
-}
-}
-}
-if (stp != 1)
-{
-hash_node_t *new_entry = malloc(sizeof(ht->size));
-if (new_entry == NULL)
-return (0);
-new_entry->key = strdup(key);
-new_entry->value = strdup(value);
-new_entry->next = ht->array[idx];
-ht->array[idx] = new_entry;
-}
-}
-}
-}
-for (r = 0; r < ht->size; r++)
-{
-if (ht->array[r] != NULL)
-{
-if (strcmp(ht->array[r]->key, key) == 0)
 return (1);
-}
-}
 return (0);
 }
