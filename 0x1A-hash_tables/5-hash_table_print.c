b56008af106296cf0dba11ed4d4a902950fd525b
freshCTRL <oyeboabariu25@gmail.com>
2023-04-16 02:51:47 -0700

    hashtables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,8 +6,9 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int i, stop, size = ht->size;
-for (stop = 0; ((size > 0) && (stop != 1)); size--)
+unsigned long int i, stop, size;
+stop = 0;
+for (size = ht->size; ((size > 0) && (stop != 1)); size--)
 {
 if (ht->array[size] != NULL)
 stop = 1;
@@ -28,7 +29,7 @@ while (ht->array[i]->next != NULL)
 {
 ht->array[i] = ht->array[i]->next;
 printf("\'%s\':", ht->array[i]->key);
-printf(" \'%s\'", ht->array[i]->value);
+printf("\'%s\'", ht->array[i]->value);
 if (i != size)
 {
 printf("%c", ',');
