3fe64bca92f2f9f4fd41712bf29cfde849146d4a
freshCTRL <oyeboabariu25@gmail.com>
2023-04-16 01:00:12 -0700

    hashtables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -36,7 +36,7 @@ while (ht->array[i]->next != NULL)
 {
 ht->array[i] = ht->array[i]->next;
 printf("\'%s\':", ht->array[i]->key);
-printf("\'%s\'", ht->array[i]->value);
+printf(" \'%s\'", ht->array[i]->value);
 if (i != size)
 {
 printf("%c", ',');
