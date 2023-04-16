99ac3e3b1d04e2a50bece7077f1edb6b4b05c4cf
freshCTRL <oyeboabariu25@gmail.com>
2023-04-16 03:15:29 -0700

    hashtables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,16 +2,14 @@
 /**
  * hash_table_delete - Entry point.
  * @ht: argument to hash_table_create.
- *
  * Description - a function that frees a hash table.
-*/
-
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 unsigned long int i;
 hash_node_t *ptr;
-if (ht != NULL)
-{
+if (ht == NULL)
+return;
 for (i = 0; i < ht->size; i++)
 {
 if (ht->array[i] != NULL)
@@ -32,5 +30,4 @@ free(ht->array);
 ht->array = NULL;
 free(ht);
 ht = NULL;
-}
 }
