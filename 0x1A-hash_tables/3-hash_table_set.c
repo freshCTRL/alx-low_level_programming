6607c05973e399eeab88485b386a746caebe446b
freshCTRL <oyeboabariu25@gmail.com>
2023-07-08 00:57:13 -0700

    hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,43 @@
 #include "hash_tables.h"
 /**
+ * insert_pair - Entry point.
+ * @ht: argument to insert_pair
+ * @idx: argument to insert_pair
+ * @key: argument to insert_pair
+ * @value: argument to insert_pair
+ * Return: 0 if failed else 1
+ */
+void insert(hash_table_t **ht, unsigned long int idx,
+const char *key, const char *value)
+{
+(*ht)->array[idx] = malloc(sizeof((*ht)->size));
+if ((*ht)->array[idx] == NULL)
+return (0);
+(*ht)->array[idx]->key = strdup(key);
+(*ht)->array[idx]->value = strdup(value);
+(*ht)->array[idx]->next = NULL;
+}
+/**
+ * insert_pair - Entry point.
+ * @ht: argument to insert_pair
+ * @idx: argument to insert_pair
+ * @key: argument to insert_pair
+ * @value: argument to insert_pair
+ * Return: 0 if failed else 1
+ */
+void insert_pair(hash_table_t **ht, unsigned long int idx,
+const char *key, const char *value)
+{
+hash_node_t *new_entry;
+new_entry = malloc(sizeof((*ht)->size));
+if (new_entry == NULL)
+return (0);
+new_entry->key = strdup(key);
+new_entry->value = strdup(value);
+new_entry->next = (*ht)->array[idx];
+(*ht)->array[idx] = new_entry;
+}
+/**
  * hash_table_set - Entry point.
  * @ht: argument to hash_table_set
  * @key:argument to hash_table_set
@@ -9,6 +47,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int idx;
+int stp = 0;
+hash_node_t *ptr = NULL;
 if (ht == NULL)
 return (0);
 if (ht->array == NULL)
@@ -19,29 +59,30 @@ idx = key_index((const unsigned char *)key, ht->size);
 if (idx < ht->size)
 {
 if (ht->array[idx] == NULL)
-{
-ht->array[idx] = malloc(sizeof(ht->size));
-if (ht->array[idx] == NULL)
-return (0);
-ht->array[idx]->key = strdup(key);
-ht->array[idx]->value = strdup(value);
-ht->array[idx]->next = NULL;
-}
+insert(&ht, idx, key, value);
 else
 {
 if (strcmp(ht->array[idx]->key, key) == 0)
-{
 ht->array[idx]->value = strdup(value);
-}
 else
 {
-hash_node_t *new_entry = malloc(sizeof(ht->size));
-if (new_entry == NULL)
-return (0);
-new_entry->key = strdup(key);
-new_entry->value = strdup(value);
-new_entry->next = ht->array[idx];
-ht->array[idx] = new_entry;
+if (ht->array[idx]->next != NULL)
+{
+ptr = ht->array[idx];
+while ((ptr != NULL) && (stp != 1))
+{
+ptr = ptr->next;
+if (strcmp(ptr->key, key) == 0)
+{
+ptr->value = strdup(value);
+stp = 1;
+}
+if ((stp != 1) && (ptr == NULL))
+insert_pair(&ht, idx, key, value);
+}
+}
+else
+insert_pair(&ht, idx, key, value);
 }
 }
 }
