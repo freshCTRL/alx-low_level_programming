6cdfbbee00cf212c856a9575aabf2def21513e27
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-07-09 07:05:14 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,36 +12,6 @@ if (ht == NULL)
 return;
 for (i = 0; i < ht->size; i++)
 {
-while (ht->array[i]->next != NULL)
-{
-ptr = ht->array[i]->next;
-free(ht->array[i]->value);
-free(ht->array[i]->key);
-free(ht->array[i]->next = NULL);
-free(ht->array[i]);
-ht->array[i] = ptr;
-}
-}
-free(ht->array);
-ht->array = NULL;
-free(ht);
-ht = NULL;
-}
-
-#include "hash_tables.h"
-/**
- * hash_table_delete - Entry point.
- * @ht: argument to hash_table_create.
- * Description - a function that frees a hash table.
- */
-void hash_table_delete(hash_table_t *ht)
-{
-unsigned long int i;
-hash_node_t *ptr = NULL;
-if (ht == NULL)
-return;
-for (i = 0; i < ht->size; i++)
-{
 while (ht->array[i] != NULL)
 {
 ptr = ht->array[i]->next;
