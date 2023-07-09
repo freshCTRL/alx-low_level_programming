d58eba7719dac0412a9a81dee25be2a8a25b877b
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-07-09 05:10:53 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,8 @@ if ((*ht)->array[idx] != NULL)
 (*ht)->array[idx]->value = strdup(value);
 (*ht)->array[idx]->next = NULL;
 }
+free((*ht)->array[idx]);
+(*ht)->array[idx] = NULL;
 }
 /**
  * insert_pair - Entry point.
