76aad012d32b5d2cd847aa0ed8eb18088a65f3eb
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-07-09 03:19:02 +0100

    hash_table_delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -16,7 +16,9 @@ while (ht->array[i] != NULL)
 {
 ptr = ht->array[i]->next;
 free(ht->array[i]->value);
+ht->array[i]->value = NULL;
 free(ht->array[i]->key);
+ht->array[i]->key = NULL;
 free(ht->array[i]->next = NULL);
 free(ht->array[i]);
 ht->array[i] = ptr;
