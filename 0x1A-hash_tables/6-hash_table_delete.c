ebbb67068c011b337c16791516770c5cde3e5eca
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-07 15:45:51 +0100

    hash_table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,36 +1,27 @@
 #include "hash_tables.h"
 /**
- * hash_table_delete - Entry point.
- * @ht: argument to hash_table_create.
- * Description - a function that frees a hash table.
- */
+* hash_table_delete - Entry point.
+* @ht: argument to hash_table_create.
+* Description - a function that frees a hash table.
+*/
 void hash_table_delete(hash_table_t *ht)
 {
 unsigned long int i;
-hash_node_t *ptr = NULL;
+hash_node_t *ptr1 = NULL, *ptr2 = NULL;
 if (ht == NULL)
 return;
 for (i = 0; i < ht->size; i++)
 {
-if (ht->array[i] != NULL)
+ptr1 = ht->array[i];
+while (ptr1)
 {
-while (ht->array[i]->next != NULL) {
-ptr = ht->array[i];
-free(ptr->value);
-free(ptr->key);
-free(ptr->next);
-ptr = NULL;
-ht->array[i] = ht->array[i]->next;
-}
-free(ht->array[i]->value);
-free(ht->array[i]->key);
-free(ht->array[i]->next);
-free(ht->array[i]);
-ht->array[i] = NULL;
+ptr2 = ptr1->next;
+free(ptr1->value);
+free(ptr1->key);
+free(ptr1);
+ptr1 = ptr2;
 }
 }
 free(ht->array);
-ht->array = NULL;
 free(ht);
-ht = NULL;
 }
