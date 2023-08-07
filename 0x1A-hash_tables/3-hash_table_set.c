2baf444536521981add60f516341d5e3b3047cb7
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-07 15:55:53 +0100

    hash_table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -49,11 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int idx;
 int stp = 0;
 hash_node_t *ptr = NULL;
-if (ht == NULL)
-return (0);
-if (ht->array == NULL)
-return (0);
-if (strcmp(key, "") == 0)
+if (ht == NULL || ht->array == NULL || strcmp(key, "") == 0)
 return (0);
 idx = key_index((const unsigned char *)key, ht->size);
 if (ht->array[idx] == NULL)
