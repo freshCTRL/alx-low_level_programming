064645ef805fbde2684cd4c00f5988062c9d495f
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-07 15:48:21 +0100

    hash_table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 /**
- * insert_pair - Entry point.
- * @ht: argument to insert_pair
- * @idx: argument to insert_pair
- * @key: argument to insert_pair
- * @value: argument to insert_pair
- * Return: 0 if failed else 1
- */
+* insert_pair - Entry point.
+* @ht: argument to insert_pair
+* @idx: argument to insert_pair
+* @key: argument to insert_pair
+* @value: argument to insert_pair
+* Return: 0 if failed else 1
+*/
 void insert(hash_table_t **ht, unsigned long int idx,
 const char *key, const char *value)
 {
-(*ht)->array[idx] = malloc(sizeof((*ht)->size));
+(*ht)->array[idx] = malloc(sizeof(hash_node_t));
 if ((*ht)->array[idx] == NULL)
 return;
 (*ht)->array[idx]->key = strdup(key);
@@ -18,18 +18,18 @@ return;
 (*ht)->array[idx]->next = NULL;
 }
 /**
- * insert_pair - Entry point.
- * @ht: argument to insert_pair
- * @idx: argument to insert_pair
- * @key: argument to insert_pair
- * @value: argument to insert_pair
- * Return: 0 if failed else 1
- */
+* insert_pair - Entry point.
+* @ht: argument to insert_pair
+* @idx: argument to insert_pair
+* @key: argument to insert_pair
+* @value: argument to insert_pair
+* Return: 0 if failed else 1
+*/
 void insert_pair(hash_table_t **ht, unsigned long int idx,
 const char *key, const char *value)
 {
 hash_node_t *new_entry;
-new_entry = malloc(sizeof((*ht)->size));
+new_entry = malloc(sizeof(hash_node_t));
 if (new_entry == NULL)
 return;
 new_entry->key = strdup(key);
@@ -38,12 +38,12 @@ new_entry->next = (*ht)->array[idx];
 (*ht)->array[idx] = new_entry;
 }
 /**
- * hash_table_set - Entry point.
- * @ht: argument to hash_table_set
- * @key:argument to hash_table_set
- * @value: argument to hash_table_set
- * Return: 0 if failed else 1
- */
+* hash_table_set - Entry point.
+* @ht: argument to hash_table_set
+* @key:argument to hash_table_set
+* @value: argument to hash_table_set
+* Return: 0 if failed else 1
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int idx;
@@ -61,7 +61,10 @@ insert(&ht, idx, key, value);
 else
 {
 if (strcmp(ht->array[idx]->key, key) == 0)
+{
+free(ht->array[idx]->value);
 ht->array[idx]->value = strdup(value);
+}
 else
 {
 if (ht->array[idx]->next != NULL)
@@ -72,6 +75,7 @@ while ((ptr->next != NULL) && (stp != 1))
 ptr = ptr->next;
 if (strcmp(ptr->key, key) == 0)
 {
+free(ptr->value);
 ptr->value = strdup(value);
 stp = 1;
 }
